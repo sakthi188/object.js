3258c87ac7f416ab2073063c07ab39f3f5209f69
sakthi188 <107858315+sakthi188@users.noreply.github.com>
2023-03-27 20:11:36 +0530

    Add files via upload

---
+++ object1.js
@@ -0,0 +1,11 @@
+var myDog={
+   "name":"coder",
+   "legs":4,
+   "tails":1,
+   "friends":["freeCoders"]
+};
+myDog.name="freeCoders max";
+console.log(myDog.name);
+
+myDog['bark']="bow bow";
+console.log(myDog.bark);
