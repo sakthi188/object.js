a01b183ba156f2604bb320e738ffae6ed3dcd7b8
sakthi188 <107858315+sakthi188@users.noreply.github.com>
2023-03-27 19:57:21 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,22 @@
+var dog={
+    "name": "Camper",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["everything!"]
+};
+
+var name=dog.name;
+var legs=dog.legs;
+console.log(name);
+console.log(legs);
+
+var test={
+    12: "Namoth",
+    16: "Montana",
+    19: "Unitas"
+};
+
+var number12=test[12];
+var number19=test[16];
+console.log(number12);
+console.log(number19);
